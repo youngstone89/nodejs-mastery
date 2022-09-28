b91a0c058687ca682469fbbb5b7d0184825e0fd8
YeongSeok Kim <youngstone89@icloud.com>
2022-09-28 18:49:43 +0900

    express.js example added

---
+++ string.js
@@ -0,0 +1,6 @@
+const text = 'Hello world, welcome to the universe.'
+const result = text.indexOf('welcome')
+console.log(result)
+
+const notfound = text.indexOf('welcome2222')
+console.log(notfound)
