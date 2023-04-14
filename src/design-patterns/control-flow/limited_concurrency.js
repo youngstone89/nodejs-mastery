ca512aad056a236778faea828c4ae6d129625870
youngstone89 <youngstone89@icloud.com>
2023-04-14 13:56:00 +0900

    applied processNextTick to task to make it asynchronous

--- src/design-patterns/control-flow/limited_concurrency.js
+++ src/design-patterns/control-flow/limited_concurrency.js
@@ -1,6 +1,8 @@
-function task (cb) {
-  console.log('task')
-  cb()
+function task (i, cb) {
+  process.nextTick(function () {
+    console.log('task', i)
+    cb()
+  })
 }
 
 const tasks = []
@@ -8,26 +10,34 @@ for (let i = 0; i < 10; i++) {
   tasks.push(task)
 }
 
-const concurrency = 2
+const concurrency = 3
 let running = 0
 let completed = 0
 let index = 0
 function next () {
   // (1)
   while (running < concurrency && index < tasks.length) {
+    console.log(`concurrency limit ${concurrency}, running ${running}`)
     const task = tasks[index++]
-    task(() => {
+    task(index, () => {
       // (2)
       if (++completed === tasks.length) {
         return finish()
       }
       running--
+      console.log(`task ${index} completed, running next`)
       next()
     })
     running++
+    console.log('currently running:', running)
   }
 }
-next()
+
 function finish () {
   console.log('finish: ', completed)
 }
+process.nextTick(next)
+
+setInterval(() => {
+  console.log(Date.now())
+}, 500)
